9e4741111ce2c4acacac59881a6e70e4b664baad
retr0cube <buthelo0@gmail.com>
2021-04-22 18:01:56 +0100

    Hmmm....

--- neptune_lib/nepMain.py
+++ neptune_lib/nepMain.py
@@ -1,64 +1,36 @@
+from pathlib import Path
 import os
-import json          #Necessary Modules For The File
-import time
-import shutil
+import json
+import shutil, stat
 
 print("""
 \033[0;34;46m🪐 Neptune API - v0.0.1_alpha - By RetroCube
 \033[0m""")
-class Name:
-    proj_name = None
-    def __init__(self,project_name):
-        self.project_name = self.proj_name
-        self.Work_Dir()   
-        time.sleep(0.5)
-        self.Dir_Exists() 
- 
-           
-class createProject: # The Main Class That Creates The Directory
-    def __init__(self,version): # Initializing The Class
+
+class createProject:
+    def __init__(self, name, version):
+        self.name = name
         self.version = version
-        print('⏳ Loading...')
-        time.sleep(1.5)
-        self.Work_Dir()
-        self.Dir_Exists()
-        print('\n\033[1;32;40m√ Done!\033[0m')
-    
-    def Work_Dir(self): # Check The Working Directory
-        wkd = str(os.getcwd())
-        if wkd != 'Project':
-           os.chdir('Project')
-           os.path.join("Project")
-        else:
-          pass
-    def Dir_Exists(self): # Checks if your Project Exists & Deletes It & Recreate it again with the new stuff
-       if not os.path.exists(f"{str(Name.proj_name)}"):
-           os.mkdir(str(Name.proj_name))
-           os.chdir(str(Name.proj_name))
-       elif os.path.exists(str(Name.proj_name)):
-           shutil.rmtree(f"{str(Name.proj_name)}/{str(Name.proj_name)}_RP" and f"{str(Name.proj_name)}/{str(Name.proj_name)}_BP" and str(Name.proj_name))
-           quit
+        os.chdir("Project")
+        self.check_dir_exist()
+        os.mkdir(str(self.name))
+        os.chdir(str(self.name))
 
-    def Res(self, cls): # The Function Creates The Ressource Pack
-        if os.getcwd() != str(Name.proj_name):
-            os.path.join(str(Name.proj_name))
-        else:
-            quit
-        if cls == True:
-           os.mkdir(f"{str(Name.proj_name)}_RP")
+    def check_dir_exist(self):
+        if os.path.exists(str(self.name)):
+            proj_dir = Path(f"{str(self.name)}/{str(self.name)}_RP" if os.path.exists()
+            proj_dir.rmdir()
         else:
             pass
 
-    def Beh(self, cls): # The Function Creates The Behavior Pack
-        if os.getcwd() != str(Name.proj_name):
-            os.path.join(str(Name.proj_name))
-        else:
-            quit
+    def Res(self, cls):
+        if os.getcwd() == str(self.name):
+            os.chdir(str(self.name))
         if cls == True:
-           os.mkdir(f"{str(Name.proj_name)}_BP")
-        else:
-            pass
+            os.mkdir("{}_RP".format(str(self.name)))
 
-
-if __name__ == "__main__": # Preventing The File from Repeating In Something Weird Happened ^_~
-    createProject()
+    def Bes(self, cls):
+        if os.getcwd() == str(self.name):
+            os.chdir(str(self.name))
+        if cls == True:
+            os.mkdir("{}_BP".format(str(self.name)))
--- project.py
+++ project.py
@@ -1,7 +1,6 @@
 import neptune_lib as Neptune
 
 
-Neptune.Name.proj_name = "My ADDON"
-Project = Neptune.createProject("1.16.210")
-Project.Beh(True)
+Project = Neptune.createProject("MHQ","1.16.210")
 Project.Res(True)
+Project.Bes(True)
