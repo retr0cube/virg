562b137c2933ea78437b6d0b6a0637e351432ef3
retr0cube <buthelo0@gmail.com>
2021-06-24 17:32:13 +0000

    Added Manifest Support & More...

--- neptune_lib/nepMain.py
+++ neptune_lib/nepMain.py
@@ -5,15 +5,14 @@
 #
 #__________________________#
 # Necessary Modules For The File #
-import random
 import neptune_lib
-import uuid
 import os
 import sys
-import time
 import json
-import shutil
+import uuid
 import logging
+
+VERS = 'v0.0.1-Alpha'
 
 try:
     apiLog = "logfile.nepLog"
@@ -28,18 +27,12 @@ except Exception:
        print(f"""\033[1;31;40m /!\ Error #002 : Unable to Create A log File (Please try Again)\033[0m
        """)
 
-
-
 print("\033[0;33;40m # You are using Python {}.{} #\033[0m \n".format(sys.version_info.major, sys.version_info.minor))
 
 print("""\033[0;34;46m🪐 Neptune API - v0.0.1_alpha - By RetroCube
 \033[0m""")
 
-# if not os.path.exists("Project"):
-#     os.mkdir("Project")
-# else:
-#     os.chdir("Project")
-#
+# Legacy Files :)
 # class createProject:
 #
 #     def __init__(self, name, version, desc):
@@ -55,168 +48,172 @@ print("""\033[0;34;46m🪐 Neptune API - v0.0.1_alpha - By RetroCube
 #         time.sleep(0.75)
 #         print(f"""   \033[0;30;47m|_ ⌛ Creating Behavior Pack...\033[0m
 #         """)
-#
-#         try:
-#             os.mkdir(str(self.name))
-#             os.chdir(str(self.name))
-#
-#         except Exception as e :
-#             print(f"""\033[1;31;40m /!\ Error : Unable to Overwrite Files (Try to Delete Them Manually)\033[0m
-#             """)
-#
 #         print(f"""\033[1;32;40m√ Done - {random.randrange(-1000000,1000000)}\033[0m
 #         """)
 #
-#     def if_check_dir_exist(self):
-#         if os.path.exists(str(self.name)):
-#             shutil.rmtree(str(self.name), ignore_errors=True)
-#         else:
-#             pass
-#
-#     def projectVersion(self, v_f, v_s, v_t):
-#         self.v_f = v_f
-#         self.v_s = v_s
-#         self.v_t = v_t
-#
-#     uuid1 = uuid.uuid1()
-#     uuid3 = uuid.uuid4()
-#
-#     def createManifest_rp(self):
-#
-#         mnifst_rp = {
-#             "format_version": 2,
-#             "header": {
-#                 "name": str(self.name),
-#                 "description": str(self.desc),
-#                 "uuid": str(self.uuid1),
-#                 "version": [
-#                     int(self.v_f),
-#                     int(self.v_s),
-#                     int(self.v_t)
-#                 ],
-#                 "min_engine_version": [
-#                     1,
-#                     13,
-#                     0
-#                 ]
-#             },
-#             "modules": [
-#                 {
-#                     "type": "resources",
-#                     "uuid": str(self.uuid3),
-#                     "version": [
-#                             1,
-#                             0,
-#                             0
-#                     ]
-#                 }
-#             ]
-#         }
-#         with open("manifest.json", "w") as manRP:
-#             json.dump(mnifst_rp, manRP, indent=4)
-#
-#     def createManifest_bp(self):
-#         mnifst_bp = {
-#         	"format_version": 2,
-#         	"header": {
-#         		"name": str(self.name),
-#         		"description": str(self.desc),
-#         		"uuid": str(self.uuid1),
-#         		"version": [
-#         			int(self.v_f),
-#         			int(self.v_s),
-#         			int(self.v_t)
-#         		],
-#         		"min_engine_version": [
-#         			1,
-#         			13,
-#         			0
-#         		]
-#         	},
-#         	"modules": [
-#         		{
-#         			"type": "data",
-#         			"uuid": str(uuid.uuid1()),
-#         			"version": [
-#         				1,
-#         				0,
-#         				0
-#         			]
-#         		}
-#         	],
-#         	"dependencies": [
-#         		{
-#         			"version": [
-#         				1,
-#         				0,
-#         				0
-#         			],
-#         			"uuid": str(self.uuid3)
-#         		}
-#         	]
-#         }
-#         with open("manifest.json", "w") as manBP:
-#              json.dump(mnifst_bp, manBP, indent=4)
-#
-#     def Res(self, cls):
-#         if os.getcwd() == str(self.name):
-#             os.chdir(str(self.name))
-#         if cls is True:
-#             os.mkdir("{}_RP".format(str(self.name)))
-#             os.chdir("{}_RP".format(str(self.name)))
-#             self.createManifest_rp()
-#
-#     def Beh(self, cls):
-#         os.chdir(f"{self.W_K_D}/{self.name}")
-#         if os.getcwd() == str(self.name):
-#             os.chdir(str(self.name))
-#         if cls is True:
-#             os.mkdir("{}_BP".format(str(self.name)))
-#             os.chdir("{}_BP".format(str(self.name)))
-#             self.createManifest_bp()
-#
 
 class Init:
+
     def __init__(self, name):
         self.name = name
-        try:
-            if not os.path.exists("Project"):
-               os.mkdir("Project")
-               os.chdir("Project")
-               os.mkdir(self.name)
-        except Exception:
-            print(f"""\033[1;31;40m /!\ Error #001 : Unable to Overwrite Files (Try to Delete Them Manually)\033[0m
-            """)
 
-    def Dependencies(self, res, beh):
+        if not os.path.exists("Project"):
+           os.mkdir("Project")
+           os.chdir("Project")
+           os.mkdir(self.name)
+        else:
+           os.chdir("Project")
+           if not os.path.exists(self.name):
+              os.mkdir(self.name)
+           else:
+              os.chdir(self.name)
 
-        if os.getcwd() == str(self.name):
-            os.chdir(str(self.name))
 
-        if res == True and beh == True:
-            logging.debug('Behavior & Ressource set to TRUE')
+    def projectVersion(self, v_f, v_s, v_t):
+        self.v_f = v_f
+        self.v_s = v_s
+        self.v_t = v_t
+
+    def createDependencies(self, res=True, beh=True):
+
+        if not os.getcwd() is str(self.name):
+            try:
+              os.chdir(str(self.name))
+            except Exception:
+              print(f"""\033[1;31;40m /!\ Error : Unable to Modify Files (Try to Delete Them Manually)\033[0m
+              """)
+              logging.debug(' Failed To Modify Files ')
+
+        if res is True and beh is True:
+            logging.info('Behavior & Ressource set to TRUE')
             folders = ['{}_res'.format(self.name), '{}_beh'.format(self.name)]
             for folder in folders:
-                os.mkdir(os.path.join(root_path, folder))
-        elif res == True and beh == False:
-            logging.debug('Ressource set to TRUE')
+                os.mkdir(os.path.join(folder))
+        elif res is True and beh is False:
+            logging.info('Ressource set to TRUE')
             os.mkdir('{}_res'.format(self.name))
-        elif res == False and beh == True:
-            logging.debug('Behavior set to TRUE')
+        elif res is False and beh is True:
+            logging.info('Behavior set to TRUE')
             os.mkdir('{}_beh'.format(self.name))
         else:
             print(f"""\033[1;31;40m /!\ Error #003 : You Need to specify at least One Dependency (Ressource Pack or Behavior Pack)\033[0m
             """)
-            logging.debug('Unable to Create Dependency \nCAUSE: UNSPECIFIED_DEPENDENCY ')
+            logging.debug(' Unable to Create Dependency \nCAUSE:root: UNSPECIFIED_DEPENDENCY ')
             quit()
 
+        os.getcwd()
 
+    def createManifest(self, isBeh, isRes, desc):
+            self.desc = desc # Déscription Amigos !
 
+            # Manifest JSON Structure fo Ressource Packs
+            mnifst_rp = {
+                "format_version": 2,
+                "header": {
+                    "name": str(self.name),
+                    "description": str(self.desc),
+                    "uuid": str(uuid.uuid1()),
+                    "version": [
+                        int(self.v_f),
+                        int(self.v_s),
+                        int(self.v_t)
+                    ],
+                    "min_engine_version": [
+                        1,
+                        13,
+                        0
+                    ]
+                },
+                "modules": [
+                    {
+                        "type": "resources",
+                        "uuid": str(uuid.uuid4()),
+                        "version": [
+                                int(self.v_f),
+                                int(self.v_s),
+                                int(self.v_t)
+                        ]
+                    }
+                ]
+            }
 
+            # Manifest JSON Structure fo Behavior Packs
+            mnifst_bp = {
+            	"format_version": 2,
+            	"header": {
+            		"name": str(self.name),
+            		"description": str(self.desc),
+            		"uuid": str(uuid.uuid4()),
+            		"version": [
+            			int(self.v_f),
+            			int(self.v_s),
+            			int(self.v_t)
+            		],
+            		"min_engine_version": [
+            			1,
+            			13,
+            			0
+            		]
+            	},
+            	"modules": [
+            		{
+            			"type": "data",
+            			"uuid": str(uuid.uuid4()),
+            			"version": [
+            				int(self.v_f),
+            				int(self.v_s),
+            				int(self.v_t)
+            			]
+            		}
+            	],
+            	"dependencies": [
+            		{
+            			"version": [
+            				int(self.v_f),
+            				int(self.v_s),
+            				int(self.v_t)
+            			],
+            			"uuid": str(uuid.uuid1())
+            		}
+            	]
+            }
 
+            CURPATH = os.getcwd() # The Current Path Variable ! Duh.
 
+            if isBeh is True and isRes is True:
+                os.chdir('{}_beh'.format(self.name)) # Changing Directory to the Ressource folder #0
+                with open("manifest.json", "w") as manBP: # Creating & Dumping the .json with Data #0
+                     json.dump(mnifst_bp, manBP, indent=4) #<-- Space Indentation #0
+                os.chdir(CURPATH) # This Will Return To The Original Directory #0
+                os.chdir('{}_res'.format(self.name))  # Changing Directory to the Ressource folder #1
+                with open("manifest.json", "w") as manRP: # Creating & Dumping the .json with Data #1
+                     json.dump(mnifst_rp, manRP, indent=4) #<-- Space Indentation #1
 
+                logging.info(' ManifestRP & BP set to TRUE') # Logging The Info into the .nepLog File #1
+                os.chdir(CURPATH) # This Will Return To The Original Directory #1
+
+            elif isBeh is False and isRes is True:
+                os.chdir('{}_res'.format(self.name))  # Changing Directory to the Ressource folder #2
+                with open("manifest.json", "w") as manRP: # Creating & Dumping the .json with Data #2
+                     json.dump(mnifst_rp, manRP, indent=4) #<-- Space Indentation #2
+
+                logging.info(' ManifestRP set to TRUE') # Logging The Info into the .nepLog File #2
+                os.chdir(CURPATH) # This Will Return To The Original Directory #2
+
+            elif isBeh is True and isRes is False:
+                os.chdir('{}_beh'.format(self.name)) # Changing Directory to the Behavior folder #3
+                with open("manifest.json", "w") as manBP: # Creating & Dumping the .json with Data #3
+                     json.dump(mnifst_rp, manBP, indent=4) #<-- Space Indentation :) #3
+
+                logging.info(' ManifestBP set to TRUE') # Logging The Info into the .nepLog File #3
+                os.chdir(CURPATH) # This Will Return To The Original Directory ;) MAGIC! #3
+
+            elif isBeh is False and isRes is False: # If Both Conditions are False The Following Error will Pop :)
+                print(f"""\033[1;31;40m /!\ Error #004 : You Need to specify at least One Manifest file\033[0m
+                """) # Printing The Error in the Terminal
+                logging.debug(' Unable to Create Manifest files \nCAUSE:root: UNSPECIFIED_MANIFEST ') # Logging The Error into the .nepLog File
+                quit() # This Will Quit The Program due to the Erro #004
 
 
 if __name__ == "main":
-    createProject()
+    Init() # This Will Make Sure That The Class Will Be Called once & Not twice <-- Bug Fixed YAY!
--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import neptune_lib as Neptune
 
-# descr = "Hi! This is My Addon "
+descr = "Hi! This is My Addon "
 #
 # Project = Neptune.createProject("myAddon","1.16.210", descr)
 # Project.projectVersion(1,0,0)
@@ -10,6 +10,8 @@ import neptune_lib as Neptune
 # Neptune.init("&","&").Item("dskd")
 
 MAIN = Neptune.Init('NeptuneDummy')
-MAIN.Dependencies(False, False)
+MAIN.createDependencies(True, True)
+MAIN.projectVersion(0,0,1)
+MAIN.createManifest(True, True ,descr)
 # Neptune.init.Res(True)
 # Neptune.init.Beh(True)
