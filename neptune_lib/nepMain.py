7d6a42b8ff05b45b18631faf1fec5fc8ce4195e3
retr0cube <buthelo0@gmail.com>
2021-07-11 15:12:55 +0000

    Made changes to the API

--- neptune_lib/nepMain.py
+++ neptune_lib/nepMain.py
@@ -15,224 +15,171 @@ import logging
 
 #__________________________#
 
+class nepCore:
 
-def createProject(name, identifier):
-    global projectName
-    global projectIdentifier
-    projectIdentifier = identifier
-    projectName = name
-    #__________________________#
+    class createProject:
 
-    if not os.path.exists('Project'):
-        os.mkdir('Project')
-    os.chdir('Project')
-    if os.path.isdir(name):
-        try:
-          # Here if the "Project" Folder Doesn't Exist It will Create it & Create The Project
-          shutil.rmtree(name, ignore_errors=True)
-          time.sleep(1)
-        except Exception:
-           print(
-               "\033[1;31;40m /!\ Error #003 : Unable to Create The Project Files (Please try to delete them properly)\033[0m\n")
-        os.mkdir(name)
-        os.chdir(name)
-    elif not os.path.isdir(name):
-        os.mkdir(name)
-        os.chdir(name)
+        projectName = None
 
-    #__________________________#
+        def __init__(self, name, version, identifier):
+            self.name = self.projectName
+            self.version = version
+            self.identifier = identifier
 
-    print("\n\033[0;36;40m Info \033[0m Creating Project... \n")
-    time.sleep(2)
+        def projectVersion(self, v_f, v_s, v_t):
+            self.v_f = v_f # The First Left Number
+            self.v_s = v_s # The Second Center Number
+            self.v_t = v_t # The Third Right Number
 
-    #__________________________#
+        def createDependencies(self, beh=True, res=True):
 
+            print("\n\033[0;36;40m Info \033[0m Creating Project... \n")
+            time.sleep(2)
 
-def projectVersion( v_f, v_s, v_t):
-    global vf, vs, vt
-    vf = v_f
-    vs = v_s
-    vt = v_t
+            if res is True and beh is True:
+                logging.info('Behavior & Ressource set to TRUE') # Logging The Info into the .nepLog File #1
+                folders = ['{}_res'.format(self.name), '{}_beh'.format(self.name)] # Sub-folders Stored in a List ;)
+                for folder in folders: # A for Loop That will Make the Two Res & Beh Directories.
+                    os.mkdir(os.path.join(folder))
+            elif res is True and beh is False:
+                logging.info('Ressource set to TRUE')
+                os.mkdir('{}_res'.format(self.name)) # Doing Pretty Much The same thing Twice
+            elif res is False and beh is True:
+                logging.info('Behavior set to TRUE')
+                os.mkdir('{}_beh'.format(self.name))
 
-
-    #__________________________#
-
-def createDependencies(beh=True, res=True):
-    global isBeh
-    global isRes
-    isBeh = beh
-    isRes = res
-
-    if res is True and beh is True:
-        folders = ['{}_res'.format(projectName), '{}_beh'.format(projectName)] # Sub-folders Stored in a List ;)
-        for folder in folders: # A for Loop That will Make the Two Res & Beh Directories.
-            os.mkdir(os.path.join(folder))
-    elif res is True and beh is False:
-        os.mkdir('{}_res'.format(projectName)) # Doing Pretty Much The same thing Twice
-    elif res is False and beh is True:
-        os.mkdir('{}_beh'.format(projectName))
-
-    else: # If None of the Conditions Occured The Following Error will Pop :)
-        print(f"""\033[1;31;40m /!\ Error #002 : You Need to specify at least One Dependency (Ressource Pack or Behavior Pack)\033[0m
-        """) # Printing The Error in the Terminal
-        logging.error(' Unable to Create Dependency \nCAUSE:root: UNSPECIFIED_DEPENDENCY ') # Logging The Error into the .nepLog File #1
-        quit() # This Will Quit The Program due to the Error #003
-
-    #__________________________#
+            else: # If None of the Conditions Occured The Following Error will Pop :)
+                print(f"""\033[1;31;40m /!\ Error #003 : You Need to specify at least One Dependency (Ressource Pack or Behavior Pack)\033[0m
+                """) # Printing The Error in the Terminal
+                logging.debug(' Unable to Create Dependency \nCAUSE:root: UNSPECIFIED_DEPENDENCY ') # Logging The Error into the .nepLog File #1
+                quit() # This Will Quit The Program due to the Error #003
 
 
-def projectProperties( desc, icon):
-        desc = desc # Descripcion Amigos !
-        icon = icon # pack_icon.png
+        def projectProperties(self, isBeh, isRes, desc, icon):
+                self.desc = desc # Descripcion Amigos !
+                self.icon = icon # pack_icon.png
 
-        #__________________________#
-
-        # Manifest JSON Structure fo Ressource Packs
-        mnifst_rp = {
-            "format_version": 2,
-            "header": {
-                "name": str(projectName),
-                "description": str(desc),
-                "uuid": str(uuid.uuid1()),
-                "version": [
-                    int(vf),
-                    int(vs),
-                    int(vt)
-                ],
-                "min_engine_version": [
-                    1,
-                    13,
-                    0
-                ]
-            },
-            "modules": [
-                {
-                    "type": "resources",
-                    "uuid": str(uuid.uuid4()),
-                    "version": [
-                            int(vf),
-                            int(vs),
-                            int(vt)
+                # Manifest JSON Structure fo Ressource Packs
+                mnifst_rp = {
+                    "format_version": 2,
+                    "header": {
+                        "name": str(self.name),
+                        "description": str(self.desc),
+                        "uuid": str(uuid.uuid1()),
+                        "version": [
+                            int(self.v_f),
+                            int(self.v_s),
+                            int(self.v_t)
+                        ],
+                        "min_engine_version": [
+                            1,
+                            13,
+                            0
+                        ]
+                    },
+                    "modules": [
+                        {
+                            "type": "resources",
+                            "uuid": str(uuid.uuid4()),
+                            "version": [
+                                    int(self.v_f),
+                                    int(self.v_s),
+                                    int(self.v_t)
+                            ]
+                        }
                     ]
                 }
-            ]
-        }
 
-        #__________________________#
+                # Manifest JSON Structure fo Behavior Packs
+                mnifst_bp = {
+                	"format_version": 2,
+                	"header": {
+                		"name": str(self.name),
+                		"description": str(self.desc),
+                		"uuid": str(uuid.uuid4()),
+                		"version": [
+                			int(self.v_f),
+                			int(self.v_s),
+                			int(self.v_t)
+                		],
+                		"min_engine_version": [
+                			1,
+                			13,
+                			0
+                		]
+                	},
+                	"modules": [
+                		{
+                			"type": "data",
+                			"uuid": str(uuid.uuid4()),
+                			"version": [
+                				int(self.v_f),
+                				int(self.v_s),
+                				int(self.v_t)
+                			]
+                		}
+                	],
+                	"dependencies": [
+                		{
+                			"version": [
+                				int(self.v_f),
+                				int(self.v_s),
+                				int(self.v_t)
+                			],
+                			"uuid": str(uuid.uuid1())
+                		}
+                	]
+                }
 
-        # Manifest JSON Structure fo Behavior Packs
-        mnifst_bp = {
-        	"format_version": 2,
-        	"header": {
-        		"name": str(projectName),
-        		"description": str(desc),
-        		"uuid": str(uuid.uuid4()),
-        		"version": [
-        			int(vf),
-        			int(vs),
-        			int(vt)
-        		],
-        		"min_engine_version": [
-        			1,
-        			13,
-        			0
-        		]
-        	},
-        	"modules": [
-        		{
-        			"type": "data",
-        			"uuid": str(uuid.uuid4()),
-        			"version": [
-        				int(vf),
-        				int(vs),
-        				int(vt)
-        			]
-        		}
-        	],
-        	"dependencies": [
-        		{
-        			"version": [
-        				int(vf),
-        				int(vs),
-        				int(vt)
-        			],
-        			"uuid": str(uuid.uuid1())
-        		}
-        	]
-        }
+                CURPATH = os.getcwd() # The Current Path Variable ! Duh.
 
-        #__________________________#
+                if isBeh is True and isRes is True:
+                    os.chdir('{}_beh'.format(self.name)) # Changing Directory to the Ressource folder
+                    with open("manifest.json", "w") as manBP: # Creating & Dumping the .json with Data
+                         json.dump(mnifst_bp, manBP, indent=4) #<-- Space Indentation
+                    os.chdir(CURPATH) # This Will Return To The Original Directory
 
-        # Manifest JSON Structure fo Behavior Packs
-        mnifst = {
-        	"format_version": 2,
-        	"header": {
-        		"name": str(projectName),
-        		"description": str(desc),
-        		"uuid": str(uuid.uuid4()),
-        		"version": [
-        			int(vf),
-        			int(vs),
-        			int(vt)
-        		],
-        		"min_engine_version": [
-        			1,
-        			13,
-        			0
-        		]
-        	},
-        	"modules": [
-        		{
-        			"type": "data",
-        			"uuid": str(uuid.uuid4()),
-        			"version": [
-        				int(vf),
-        				int(vs),
-        				int(vt)
-        			]
-        		}
-        	]
-         }
+                    os.chdir('{}_res'.format(self.name))
+                    os.mkdir('textures')
+                    shutil.copy(self.icon, CURPATH) # Copying the pack_icon.png image to the destination
+                    with open("blocks.json", "w") as manRP: # # Creating & Dumping the blocks.json with Data
+                         json.dump({}, manRP, indent=4)
+                    with open("manifest.json", "w") as manRP: # Doing The Same Thing again...
+                         json.dump(mnifst_rp, manRP, indent=4)
 
-        #__________________________#
+                    logging.info(' ManifestRP & BP set to TRUE')
+                    os.chdir(CURPATH)
 
-        curPath = os.getcwd() # The Current Path Variable, Duh.
+                elif isBeh is False and isRes is True:
+                    os.chdir('{}_res'.format(self.name))
+                    os.mkdir('textures')
+                    shutil.copy(self.icon, CURPATH) # Copying the pack_icon.png image to the destination
+                    with open("blocks.json", "w") as manRP: # Still Doing it...
+                         json.dump({}, manRP, indent=4)
+                    with open("manifest.json", "w") as manRP: # Still Doing it...
+                         json.dump(mnifst_rp, manRP, indent=4)
 
-        #__________________________#
+                    logging.info(' ManifestRP set to TRUE')
+                    os.chdir(CURPATH)
 
-        if isBeh is True and isRes is True:
-            os.chdir('{}_beh'.format(projectName)) # Changing Directory to the Ressource folder
-            with open("manifest.json", "w") as manBP: # Creating & Dumping the .json with Data
-                 json.dump(mnifst_bp, manBP, indent=4) #<-- Space Indentation
-            os.chdir(curPath) # This Will Return To The Original Directory
-            os.chdir('{}_res'.format(projectName))
-            os.mkdir('textures')
-            shutil.copy(icon, curPath) # Copying the pack_icon.png image to the destination
-            with open("blocks.json", "w") as manRP: # Creating & Dumping the blocks.json with Data
-                 json.dump({}, manRP, indent=4)
-            with open("manifest.json", "w") as manRP: # Doing The Same Thing again...
-                 json.dump(mnifst_rp, manRP, indent=4)
+                elif isBeh is True and isRes is False:
+                    os.chdir('{}_beh'.format(self.name))
+                    with open("manifest.json", "w") as manBP:
+                         json.dump(mnifst_rp, manBP, indent=4) # Finally! =)
 
-        elif isBeh is False and isRes is True:
-            os.chdir('{}_res'.format(projectName))
-            os.mkdir('textures')
-            shutil.copy(icon, curPath) # Copying the pack_icon.png image to the destination
-            with open("blocks.json", "w") as manRP: # Still Doing it...
-                 json.dump({}, manRP, indent=4)
-            with open("manifest.json", "w") as manRP: # Still Doing it...
-                 json.dump(mnifst_rp, manRP, indent=4)
+                    logging.info(' ManifestBP set to TRUE')
+                    os.chdir(CURPATH)
 
-        elif isBeh is True and isRes is False:
-            os.chdir('{}_beh'.format(projectName))
-            with open("manifest.json", "w") as manBP:
-                 json.dump(mnifst, manBP, indent=4) # Finally! =)
+                elif isBeh is False and isRes is False: # If Both Conditions are False The Following Error will Pop :)
+                    print(f"""\033[1;31;40m /!\ Error #004 : You Need to specify at least One Manifest file\033[0m
+                    """) # Printing The Error in the Terminal
+                    logging.debug(' Unable to Create Manifest files \nCAUSE:root: UNSPECIFIED_MANIFEST ') # Logging The Error into the .nepLog File
+                    quit() # This Will Quit The Program due to the Error #004
 
-        #__________________________#
+    print(createProject.projectName)
 
-        os.chdir(curPath) # This Will Return To The Original Directory
 
-        #__________________________#
 
 
 if __name__ == "main":
-    createProject() # This Will Make Sure That The Class Will Be Called once & Not twice <-- Bug Fixed YAY!
+    nepCore() # This Will Make Sure That The Class Will Be Called once & Not twice <-- Bug Fixed YAY!
--- project.py
+++ project.py
@@ -4,15 +4,16 @@ import neptune_lib as Neptune
 descr = "Hi! This is a Dummy " # This is The Description Variable for your project
 icon = "pack_icon.png" # The "pack_icon.png" Path for your project
 
+add_on = Neptune.nepCore.createProject('MYADDON',"1.13.4",'mya')
+
 #_____Project Properties____#
 
-add_on = Neptune.createProject('NeptuneDummy', 'nepy')  #This Class Will create the project
-add_on.createDependencies(True, True) # This Method create the Behavior Pack/Ressource Pack
+# add_on.createDependencies(True, True) # This Method create the Behavior Pack/Ressource Pack
 
 #_____Manifest Properties____#
-
-add_on.projectVersion(0,1,1) # The Project Version e.g('1.0.0','4.5.3')
-add_on.projectProperties(descr, icon) # Here You put a description & an icon for your project
+#
+# add_on.projectVersion(0,1,1) # The Project Version e.g('1.0.0','4.5.3')
+# add_on.projectProperties(descr, icon) # Here You put a description & an icon for your project
 
 #_____Object Creation____#
 
