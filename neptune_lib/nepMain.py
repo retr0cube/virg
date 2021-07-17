d5452b29898b0e73cf53f6621ff303094da2c294
retr0cube <buthelo0@gmail.com>
2021-07-17 15:01:50 +0000

    Reimplemented the logging system...

--- neptune_lib/nepMain.py
+++ neptune_lib/nepMain.py
@@ -5,6 +5,10 @@
 #
 #________Modules__________#
 
+# from __future__ import annotations
+# from typing import List, Dict, Union, Optional
+# import attr
+# import cattr
 import sys
 import json
 import uuid
@@ -14,6 +18,20 @@ import time
 import send2trash
 import os
 import logging
+
+#______Logging Stuff_______#
+
+try:
+    apiLog = "logfile.nepLog"
+    if os.path.exists(apiLog):
+        os.remove(apiLog)
+    for handler in logging.root.handlers[:]: # This will log Errors/Issues
+        logging.root.removeHandler(handler)
+    logging.basicConfig(filename=apiLog,level=logging.DEBUG)
+    logging.info(' nepMain.py started...')
+    logging.debug(' loading components...\n')
+except Exception:
+       print("\033[1;31;40m /!\ Error #001 : Unable to Create A log File (Please try Again)\033[0m\n")
 
 #__________________________#
 
@@ -26,16 +44,20 @@ class createProject:
 
     def __init__(createProject, name, identifier):
         global projectName
+        global projectIdentifier
         projectName = createProject.name = name # The Project Name Variable =I
-        createProject.identifier = identifier # The Identifier Name Variable
+        projectIdentifier = createProject.identifier = identifier # The Identifier Name Variable
 
         #__________________________#
 
         print("\n\033[0;36;40m Info \033[0m Creating Project... \n")
+        logging.debug(" projectName = {}".format(str(projectName)))
+        logging.debug(" projectIdentifier = {}\n".format(str(projectIdentifier)))
         time.sleep(2)
 
         #__________________________#
         try:
+            logging.info(" creating project...")
             if not os.path.exists('Project'):
                 os.mkdir('Project')
             os.chdir('Project')
@@ -46,14 +68,19 @@ class createProject:
             elif not os.path.isfile(createProject.name):
                 os.mkdir(createProject.name)
                 os.chdir(createProject.name)
+            logging.info(" task ended successfully!")
         except Exception :
-           print("\033[1;31;40m /!\ Error #003 : Unable to Create The Project (Please try to delete it properly)\033[0m\n")
-
+            logging.info(" task ended with an Exception!")
+            logging.error(" an error occured while creating the project, please try again!")
+            logging.info(" exited with error #003")
+            print("\033[1;31;40m /!\ Error #003 : Unable to Create The Project (Please try to delete it properly)\033[0m\n")
+            quit()
 
     def projectVersion(createProject, v_f, v_s, v_t):
         createProject.v_f = v_f # The First Left Number
         createProject.v_s = v_s # The Second Center Number
         createProject.v_t = v_t # The Third Right Number
+        logging.debug(" version = v{}.{}.{}".format(createProject.v_f,createProject.v_s,createProject.v_t))
 
         #__________________________#
 
@@ -64,15 +91,15 @@ class createProject:
         #__________________________#
 
         if res is True and beh is True:
-            logging.info('Behavior & Ressource set to TRUE') # Logging The Info into the .nepLog File #1
+            logging.info(' beh = True ; res = True') # Logging The Info into the .nepLog File #1
             folders = ['{}_res'.format(createProject.name), '{}_beh'.format(createProject.name)] # Sub-folders Stored in a List ;)
             for folder in folders: # A for Loop That will Make the Two Res & Beh Directories.
                 os.mkdir(os.path.join(folder))
         elif res is True and beh is False:
-            logging.info('Ressource set to TRUE')
+            logging.info(' res = true')
             os.mkdir('{}_res'.format(createProject.name)) # Doing Pretty Much The same thing Twice
         elif res is False and beh is True:
-            logging.info('Behavior set to TRUE')
+            logging.info(' beh = True')
             os.mkdir('{}_beh'.format(createProject.name))
 
         else: # If None of the Conditions Occured The Following Error will Pop :)
@@ -99,8 +126,8 @@ class createProject:
                     "uuid": str(uuid.uuid1()),
                     "version": [
                         int(createProject.v_f),
-                        int(createProject.v_f),
-                        int(createProject.v_f)
+                        int(createProject.v_s),
+                        int(createProject.v_t)
                     ],
                     "min_engine_version": [
                         1,
@@ -114,8 +141,8 @@ class createProject:
                         "uuid": str(uuid.uuid4()),
                         "version": [
                                 int(createProject.v_f),
-                                int(createProject.v_f),
-                                int(createProject.v_f)
+                                int(createProject.v_s),
+                                int(createProject.v_t)
                         ]
                     }
                 ]
@@ -132,8 +159,8 @@ class createProject:
             		"uuid": str(uuid.uuid4()),
             		"version": [
             			int(createProject.v_f),
-            			int(createProject.v_f),
-            			int(createProject.v_f)
+            			int(createProject.v_s),
+            			int(createProject.v_t)
             		],
             		"min_engine_version": [
             			1,
@@ -147,8 +174,8 @@ class createProject:
             			"uuid": str(uuid.uuid4()),
             			"version": [
             				int(createProject.v_f),
-            				int(createProject.v_f),
-            				int(createProject.v_f)
+            				int(createProject.v_s),
+            				int(createProject.v_t)
             			]
             		}
             	],
@@ -156,8 +183,8 @@ class createProject:
             		{
             			"version": [
             				int(createProject.v_f),
-            				int(createProject.v_f),
-            				int(createProject.v_f)
+            				int(createProject.v_s),
+            				int(createProject.v_t)
             			],
             			"uuid": str(uuid.uuid1())
             		}
@@ -175,8 +202,8 @@ class createProject:
             		"uuid": str(uuid.uuid4()),
             		"version": [
             			int(createProject.v_f),
-            			int(createProject.v_f),
-            			int(createProject.v_f)
+            			int(createProject.v_s),
+            			int(createProject.v_t)
             		],
             		"min_engine_version": [
             			1,
@@ -190,8 +217,8 @@ class createProject:
             			"uuid": str(uuid.uuid4()),
             			"version": [
             				int(createProject.v_f),
-            				int(createProject.v_f),
-            				int(createProject.v_f)
+            				int(createProject.v_s),
+            				int(createProject.v_t)
             			]
             		}
             	]
@@ -199,7 +226,7 @@ class createProject:
 
             #__________________________#
 
-            curPath = os.getcwd() # The Current Path Variable, Duh.
+            projectRoot = os.getcwd() # The Current Path Variable, Duh.
 
             #__________________________#
 
@@ -207,16 +234,15 @@ class createProject:
                 os.chdir('{}_beh'.format(createProject.name)) # Changing Directory to the Ressource folder
                 with open("manifest.json", "w") as manBP: # Creating & Dumping the .json with Data
                      json.dump(mnifst_bp, manBP, indent=4) #<-- Space Indentation
-                shutil.copy(createProject.icon, curPath) # Copying the pack_icon.png image to the destination
-                os.chdir(curPath) # This Will Return To The Original Directory
+                     shutil.copy(createProject.icon, os.getcwd()) # Copying the pack_icon.png image to the destination
+                os.chdir(projectRoot) # This Will Return To The Original Directory
                 os.chdir('{}_res'.format(createProject.name))
                 os.mkdir('textures')
                 with open("blocks.json", "w") as manRP: # Creating & Dumping the blocks.json with Data
                      pass
                 with open("manifest.json", "w") as manRP: # Doing The Same Thing again...
                      json.dump(mnifst_rp, manRP, indent=4)
-                shutil.copy(createProject.icon, curPath) # Copying the pack_icon.png image to the destination
-                logging.info(' ManifestRP & BP set to TRUE')
+                     shutil.copy(createProject.icon, os.getcwd()) # Copying the pack_icon.png image to the destination
 
             elif createProject.beh is False and createProject.res is True:
                 os.chdir('{}_res'.format(createProject.name))
@@ -225,50 +251,22 @@ class createProject:
                      pass
                 with open("manifest.json", "w") as manRP: # Still Doing it...
                      json.dump(mnifst_rp, manRP, indent=4)
-                shutil.copy(createProject.icon, curPath) # Copying the pack_icon.png image to the destination
-                logging.info(' ManifestRP set to TRUE')
+
+                shutil.copy(createProject.icon, os.getcwd()) # Copying the pack_icon.png image to the destination
 
             elif createProject.beh is True and createProject.res is False:
                 os.chdir('{}_beh'.format(createProject.name))
-                shutil.copy(createProject.icon, curPath)
                 with open("manifest.json", "w") as manBP:
                      json.dump(mnifst, manBP, indent=4) # Finally! =)
-                logging.info(' ManifestBP set to TRUE')
+                shutil.copy(createProject.icon, os.getcwd())
+
+            logging.info(" created manifest files successfully!")
 
             #__________________________#
 
             os.chdir(mainPath) # This Will Return To The Original Directory
 
 #__________________________#
-
-# class createTexture:
-#
-#         def __init__(self, textureType, textureName, path):
-#             self.textureType = textureType
-#             self.textureName = textureName
-#             self.path = path
-#
-#             if self.textureType is 'BLOCK':
-#                 os.chdir('Project')
-#                 os.chdir(projectName)
-#                 os.chdir("{}_res".format(projectName))
-#                 formatBlockJson ={
-#                  "format_version":[
-#                     		1,
-#                     		1,
-#                     		0
-#                     	],
-#                       }
-#
-#                 with open('blocks.json','r+') as block:
-#                     json.dump(formatBlockJson, block, indent=4)
-#
-#                 blockJson = {
-
-
-
-
-
 
 
 if __name__ == "main":
