2b5a6537ba547abcf90e0d2e2ac249e215f715ab
retr0cube <buthelo0@gmail.com>
2021-07-31 16:51:47 +0000

    ``

--- neptune_lib/__main__.py
+++ neptune_lib/__main__.py
@@ -17,7 +17,7 @@ api_version = 'v0.0.2-p:alpha:3' # API's Version
 
 print("\033[1;33;40m Python Info \033[0m\033[1;30;40m- \033[0mYou are Using Python {}.{} \n".format(sys.version_info.major, sys.version_info.minor))
 
-print("\033[0;36;40m Info \033[0m\033[1;30;40m- \033[0m 🌚 Neptune API by Retr0cube")
+print("\033[0;36;40m Info \033[0m\033[1;30;40m- \033[0m Neptune API by Retr0cube")
 
 print("\033[1;35;40m Version \033[0m\033[1;30;40m- \033[0m {}\n".format(api_version))
 
