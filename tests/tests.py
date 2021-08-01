d49ebf9db4d6dc22ed51c74c26d98c6513d4ebdb
retr0cube <buthelo0@gmail.com>
2021-08-01 18:17:24 +0000

    Update config.yaml

---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+#_____The API Module____#
+import neptune_lib as Neptune
+import os
+
+#___________________________#
+
+script_path = os.path.dirname(__file__)
+
+#___________________________#
+
+descr = "Hi! This is My add-on" # This is The Description Variable for your project
+icon = os.path.join("{}/src".format(script_path), 'pack_icon.png') # The "pack_icon.png" Path for your project
+
+#_____Project Properties____#
+
+add_on = Neptune.create_project.CreateProject( "myaddon","mya")  #This Class Will create the project
+add_on.create_dependencies(True, True) # This Method create the Behavior Pack/Ressource Pack
+
+#_____Manifest Properties____#
+
+add_on.project_version([1,7,1]) # The Project Version e.g('1.0.0','4.5.3')
+add_on.project_properties(descr, icon) # Here You put a description & an icon for your project
+
+
+# Neptune.createTexture("BLOCK","blockdummy","textureconng"
