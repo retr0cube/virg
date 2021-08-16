0420eaf91a614dc513052c406e4b2028a8718de3
Retcy <buthelo0@gmail.com>
2021-08-16 14:54:12 +0000

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,34 +14,11 @@ script_path = os.path.dirname(__file__)
 #___________________________#
 
 descr = "Hi! This is My add-on" # This is The Description Variable for your project
-icon = open("{}/src/pack_icon.png".format(script_path)) # The "pack_icon.png" Path for your project
+icon = open(f"{script_path}/src/pack_icon.png") # The "pack_icon.png" Path for your project
 
 #_____Project Properties____#
 
 add_on = create_project.CreateProject( "myaddon","mya")  #This Class Will create the project
-add_on.create_dependencies(True, True) # This Method create the Behavior Pack/Ressource Pack
-
-#_____Manifest Properties____#
-
-add_on.project_version([1,7,1]) # The Project Version e.g('1.0.0','4.5.3')
-add_on.project_properties(descr, icon) # Here You put a description & an icon for your project
-
-#_____The API Module____#
-
-import neptune_lib
-
-from neptune_lib.core.create_project import *
-from neptune_lib.core.create_texture import *
-from neptune_lib.components import textures
-
-#___________________________#
-
-descr = "Hi! This is My add-on" # This is The Description Variable for your project
-icon = r"C:\Users\Retcy\Downloads\2. Compressed\Vanilla_Resource_Pack_1.16.200\pack_icon.png" # The "pack_icon.png" Path for your project
-
-#_____Project Properties____#
-
-add_on = CreateProject( "myaddon","mya")  #This Class Will create the project
 add_on.create_dependencies(True, True) # This Method create the Behavior Pack/Ressource Pack
 
 #_____Manifest Properties____#
